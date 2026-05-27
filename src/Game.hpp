33268ec8d7902f9fb39a8d1fb2f3e868b56ae7ae
Ishan <jishnukaushik16@gmail.com>
2026-05-27 07:09:41 +0530

    Add TrainingHubState and LessonViewerState to Game class and update game state management

--- src/Game.hpp
+++ src/Game.hpp
@@ -13,21 +13,29 @@ class TrainingState;
 class ProfileSelectionState;
 class CharacterSelectionState;
 class SplashState;
+class TrainingHubState;
+class LessonViewerState;
 
 class Game {
 private:
     sf::RenderWindow window;
     sf::Font mainFont;
     bool isRunning;
-    
+
     std::unique_ptr<MenuState> menuState;
     std::unique_ptr<RegistrationState> registrationState;
     std::unique_ptr<TrainingState> trainingState;
     std::unique_ptr<ProfileSelectionState> profileSelectionState;
     std::unique_ptr<CharacterSelectionState> characterSelectionState;
     std::unique_ptr<SplashState> splashState;
-    
-    enum class GameState { SPLASH, MENU, REGISTRATION, TRAINING, GAMEPLAY, PROFILE_SELECTION, CHARACTER_SELECTION };
+    std::unique_ptr<TrainingHubState> trainingHubState;
+    std::unique_ptr<LessonViewerState> lessonViewerState;
+
+    enum class GameState {
+        SPLASH, MENU, REGISTRATION, TRAINING, GAMEPLAY,
+        PROFILE_SELECTION, CHARACTER_SELECTION,
+        TRAINING_HUB, LESSON_VIEWER
+    };
     GameState currentGameState;
     
     void initWindow();
@@ -50,6 +58,8 @@ public:
     void switchToGameplay();
     void switchToProfileSelection();
     void switchToCharacterSelection();
+    void switchToTrainingHub();
+    void switchToLessonViewer();
     
     sf::RenderWindow& getWindow() { return window; }
     sf::Font& getMainFont() { return mainFont; }
@@ -58,4 +68,8 @@ public:
     PlayerData playerData;
     bool hasLoadedProfile;
     int selectedCharacterId;
+
+    // Set by TrainingHubState before switching to LessonViewerState
+    std::string trainingChapterPath;
+    std::string trainingChapterTitle;
 };
